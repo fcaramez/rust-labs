32c8c6b3742386d01792e1815105922da88c0d19
fcaramez <fcaramez@DESKTOP-URF1JVA>
2023-04-12 01:54:53 +0100

    adds first iterations

--- functions-and-arrays/src/main.rs
+++ functions-and-arrays/src/main.rs
@@ -1,8 +1,47 @@
-fn max_of_two_numbers() {}
+fn max_of_two_numbers(number1: i32, number2: i32) -> i32 {
+    if number1 > number2 {
+        return number1;
+    } else {
+        return number2;
+    }
+}
 
-fn find_longest_word() {}
+const WORDS: [&str; 10] = [
+    "item",
+    "vote",
+    "return",
+    "prove",
+    "ridge",
+    "social",
+    "recommendation",
+    "terrace",
+    "provide",
+    "advocate",
+];
 
-fn sum_numbers() {}
+fn find_longest_word<'a>(arr: &'a [&'a str]) -> &'a str {
+    let mut longest_word = "";
+
+    for word in arr {
+        if word.len() > longest_word.len() {
+            longest_word = word;
+        }
+    }
+
+    longest_word
+}
+
+const NUMBERS: [i32; 10] = [234, 21354, 123, 1243, 213, 645, 756, 657, 546, 5];
+
+fn sum_numbers<'a>(arr: &'a [i32]) -> i32 {
+    let mut full_sum = 0;
+
+    for num in arr {
+        full_sum += num
+    }
+
+    full_sum
+}
 
 fn sum() {}
 
@@ -20,7 +59,11 @@ fn how_many_times() {}
 
 fn greatest_product() {}
 
-
 fn main() {
-    println!("Functions and arrays lab");
+    let biggest = max_of_two_numbers(142, 453);
+    let longest = find_longest_word(&WORDS);
+    let sum = sum_numbers(&NUMBERS);
+    println!(
+        "Functions and arrays lab, biggest number is {biggest}, longest is {longest}, sum is {sum}"
+    );
 }
